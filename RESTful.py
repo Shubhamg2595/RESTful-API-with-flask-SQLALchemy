52a9276bf24567ff627b80e87835f8399d6aa725
Shubham Gupta <shubhamg2595@gmail.com>
2018-09-25 21:42:07 +0530

    Required SQLALCHEMY SETUP DONE!

--- RESTful.py
+++ RESTful.py
@@ -6,21 +6,28 @@ from resources.user import UserRegistration
 from resources.Item import Item,ItemList
 app=Flask(__name__)
 'create a secret key for your app'
+'''
+'important to specify this property for sqlAlchemy' \
+'as sqlalchemy is always tracking the changes that' \
+'we make to our database, but that consumes a lot' \
+'of resources' 
+'''
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 app.secret_key='shubham:)'
 api=Api(app)
 
 jwt=JWT(app,authenticate,identity)
 
 
-'''
-in this commit,
-i will make the entire item class storage and retrieval process with database
-'''
-
-
-
 api.add_resource(Item,'/item/<string:name>')
 api.add_resource(ItemList,'/items')
 api.add_resource(UserRegistration,'/register')
+'''REASON I IMPORTED THE 'db' here is because of
+    circular imports
+    '''
 if __name__ == '__main__':
-    app.run(port=5000,debug=True)
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
+
--- create_tables.py
+++ create_tables.py
@@ -7,7 +7,7 @@ cursor=connection.cursor()
 create_table= "create table if not exists users(id INTEGER PRIMARY KEY,username text,password text)"
 cursor.execute(create_table)
 
-create_item_table="create table if not exists items(name text,price real)"
+create_item_table="create table if not exists items(id INTEGER PRIMARY KEY,name text,price real)"
 cursor.execute(create_item_table)
 
 # 'inserting a sample item'
